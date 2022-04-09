0caf9918ea133a75c4f90a9be7f1f8707c7945f4
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-09 14:54:23 -0700

    return to mod 13 votes and comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -44,11 +44,57 @@ router.get('/', (req, res) => {
             //     username: 'test_user'
             // }
             // console.log(dbPostData[0]);
-            //DBPOSTDATA IS THEN HANDLEBARRED AS POSTS-BUT YOU NEED TO UPDATE THE HOMEPAGE.HBS CODE
+            //DBPOSTDATA IS THEN HANDLEBARRED AS POSTS-BUT YOU NEED TO UPDATE THE HOMEPAGE.HANDLEBAR CODE
 
             //MAKE AN ARRAY OF THE JAVASCRIPT OBJECTS FOR HANDLEBARS TO USE
             const posts = dbPostData.map(post => post.get({ plain: true }));
             res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//HOME-ROUTE FOR THE SINGLE-POST HANDLEBAR
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            // {
+            //     model: Comment,
+            //     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            //     include: {
+            //         model: User,
+            //         attributes: ['username']
+            //     }
+            // },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            //SERIALIZE TO PERSIST IN STORAGE, TRANSFER, AND DISTRIBUTE
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('single-post', { post });
         })
         .catch(err => {
             console.log(err);
