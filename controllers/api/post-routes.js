84db5d50d6ab1d39d1ab420743f7e01306f3a046
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-09 18:21:33 -0700

    insomnia testing

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,5 @@
+//IMPORT CONNECTION TO THE DB
+const sequelize = require('../../config/connection');
 //ADD LIBS
 const router = require('express').Router();
 const { Router } = require('express');
@@ -69,9 +71,31 @@ router.put('/upvote', (req, res) => {
     Vote.create({
         user_id: req.body.user_id,
         post_id: req.body.post_id
-    })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => res.json(err));
+    }).then(() => {
+        //FIND VOTED ON POST
+        return Post.findOne({
+            where: {
+                id: req.body.post_id
+            },
+            attributes: [
+                'id',
+                'post_url',
+                'title',
+                'created_at',
+                //USE MYSQL QUERY TO GET VOTE COUNT UNDER VAR NAME VOTE_COUNT
+                [
+                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                    'vote_count',
+                ]
+            ]
+        })
+            .then(dbPostData => res.json(dbPostData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+
+            });
+    });
 });
 
 router.put('/:id', (req, res) => {
