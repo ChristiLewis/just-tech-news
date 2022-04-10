478aefbccd49b2ba83ee8ac02e4c9bd0b62c9ac3
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-10 14:57:27 -0700

    single post linked via comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 //IMPORT MODULES AND MODELS-NOTE LEAVE OUT COMMENT MODEL TO ADD IN LATER const { Post, User, Comment } = require('../models');
 const sequelize = require('../config/connection');
-const { Post, User } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
 router.get('/', (req, res) => {
     console.log(req.session);
@@ -70,14 +70,14 @@ router.get('/post/:id', (req, res) => {
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
-            // {
-            //     model: Comment,
-            //     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            //     include: {
-            //         model: User,
-            //         attributes: ['username']
-            //     }
-            // },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -93,7 +93,7 @@ router.get('/post/:id', (req, res) => {
             //SERIALIZE TO PERSIST IN STORAGE, TRANSFER, AND DISTRIBUTE
             const post = dbPostData.get({ plain: true });
 
-            // pass data to template
+            // PASS DATA TO TEMPLATE
             res.render('single-post', { post });
         })
         .catch(err => {
