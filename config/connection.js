c115e15a40bbe07091670b95fc4332a7a65e0206
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-10 09:49:22 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 //IMPORT SEQUELIZE CONSTRUCTOR FROM THE LIB
 const Sequelize = require('sequelize');
 
-// CONNECT DB - PASSED IN USERNAME AND PASSWORD- REPLACED AND BETTER PROTECTED BY THE .ENV FILE
+// CONNECT DB - PASSED IN USERNAME AND PASSWORD- REPLACED AND BETTER PROTECTED BY THE .ENV FILE-CHANGED TO JAWSDB FOR HEROKU DEPLOYMENT
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
+}
 
 module.exports = sequelize;
